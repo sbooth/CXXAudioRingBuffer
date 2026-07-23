5ef4f56e72e13ee11af6edcfffdb39491f51d0ee
Stephen Booth <me@sbooth.org>
2026-07-23 08:36:20 -0500

    Update tests

--- Tests/CXXAudioRingBufferTests/CXXAudioRingBufferTests.swift
+++ Tests/CXXAudioRingBufferTests/CXXAudioRingBufferTests.swift
@@ -13,21 +13,21 @@ import Testing
         let empty = spsc.AudioRingBuffer()
         #expect(empty.__convertToBool() == false)
         #expect(empty.capacity() == 0)
-        #expect(empty.availableFrames() == 0)
-        #expect(empty.freeSpace() == empty.capacity())
+        #expect(empty.availableToRead() == 0)
+        #expect(empty.availableToWrite() == empty.capacity())
 
         var rb = spsc.AudioRingBuffer()
         let std2ch = AudioStreamBasicDescription(mSampleRate: 44100, mFormatID: kAudioFormatLinearPCM, mFormatFlags: kAudioFormatFlagsNativeFloatPacked|kAudioFormatFlagIsNonInterleaved, mBytesPerPacket: 8, mFramesPerPacket: 8, mBytesPerFrame: 8, mChannelsPerFrame: 2, mBitsPerChannel: 32, mReserved: 0)
         #expect(rb.allocate(std2ch, 512) == true)
         #expect(rb.__convertToBool() == true)
         #expect(rb.capacity() == 512)
-        #expect(rb.availableFrames() == 0)
-        #expect(rb.freeSpace() == rb.capacity())
+        #expect(rb.availableToRead() == 0)
+        #expect(rb.availableToWrite() == rb.capacity())
 
         rb.deallocate()
         #expect(rb.__convertToBool() == false)
         #expect(rb.capacity() == 0)
-        #expect(rb.availableFrames() == 0)
-        #expect(rb.freeSpace() == rb.capacity())
+        #expect(rb.availableToRead() == 0)
+        #expect(rb.availableToWrite() == rb.capacity())
     }
 }
